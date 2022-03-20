8e032c51df3c18c24a07852b1bdac293e1d3d7fb
aprilmadaha <lgx71@qq.com>
2022-03-20 22:43:06 +0800

    Update autoping.py

--- autoping.py
+++ autoping.py
@@ -10,7 +10,7 @@ def get_ping_list(filename, sheet): #从表里读取objec列的所有数值
 def write_to_excel(filename, sheet, ncols, write_result):#写入excel并保存
     wb = pd.read_excel(filename,sheet)
     wb.insert(loc=ncols, column=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),value=write_result, allow_duplicates=True)#新增一列并插入相应的数值
-    wb.to_excel("/Users/linguangxing/Desktop/pingping/pandacopy/autoping3.xlsx")
+    wb.to_excel("./autoping3.xlsx")
 
 def get_result(result): #得到结果
     if result:
@@ -18,7 +18,7 @@ def get_result(result): #得到结果
     else:
         return 1 #存活返回1
 
-excel_name = '/Users/linguangxing/Desktop/pingping/pandacopy/autoping.xlsx'
+excel_name = './autoping.xlsx'
 excel_sheet = 'Sheet1'
 ping_list = get_ping_list(excel_name, excel_sheet)
 result_list = []
